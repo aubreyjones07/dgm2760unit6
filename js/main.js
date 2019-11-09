47ae3808c85b0452b62226e7eb94dba63c32086a
Aubrey Jones <aubrey@Deannas-MacBook-Pro.local>
2019-11-09 16:00:18 -0700

    fixed textarea

--- js/main.js
+++ js/main.js
@@ -5,10 +5,6 @@
 
 
 
-function loadImage() {
-    
-}
-
 
 
 var companyTitle = "Nonsense Story";
@@ -26,42 +22,37 @@ function theStory() {
 
 
 
-const thenouns = document.querySelector('#nouns').innerHTML;
-const lowerNoun = thenouns.toLowerCase(); 
-    const nounWords = thenouns.split(' ');
-    const chars = thenouns.split('');
-    console.log(nounWords[1]); 
-const nW = ',';
-const nWords = lowerNoun.split(nW); 
-console.log(nWords);  
+const thenouns = document.querySelector('#nouns').value;
+const lowerNoun = thenouns.toLowerCase();   
+const spacecommaSpecial = /[\s,\W ]+/g;
+const nounWords = lowerNoun.trim().split(spacecommaSpecial);
+console.log(nounWords); 
+const upperNoun = thenouns.toUpperCase(); 
+const chars = upperNoun.split('');
+
+
+const theadjs = document.querySelector('#adjs').value; 
+const lowerAdjs = theadjs.toLowerCase(); 
+const spacecommaSpecialA = /[\s,\W ]+/g;
+const adjsWords = lowerAdjs.trim().split(spacecommaSpecialA);
+console.log(adjsWords); 
  
 
 
-
-const theadjs = document.querySelector('#adjs').innerHTML; 
-const lowerAdjs = theadjs.toLowerCase(); 
-const adjsWords = theadjs.split(' ');
-console.log(adjsWords[1]); 
-const aW = ',';
-const aWords = lowerAdjs.split(aW);  
-console.log(aWords); 
-
-
-const theverbs = document.querySelector('#verbs').innerHTML;  
+const theverbs = document.querySelector('#verbs').value;  
 const lowerVerb = theverbs.toLowerCase(); 
-const verbsWords = theverbs.split(' ');
-console.log(verbsWords[1]); 
-const vW = ',';
-const vWords = lowerVerb.split(vW); 
-console.log(vWords); 
+const spacecommaSpecialV = /[\s,\W ]+/g;
+const verbsWords = lowerVerb.trim().split(spacecommaSpecialV);
+console.log(verbsWords); 
+
     
 
-const myStory1 = `Once upon a time, there were four ${nWords[0]}s named ${chars[0]}andyn, ${chars[0]}yler, ${chars[0]}avin, and ${chars[0]}andie. `,
-myStory2 = `They were best friends for life and wanted a ${aWords[0]} adventure. They planned to ${vWords[0]}` ,   
-myStory3 = ` to the lost Treasure of Titan. They had two ${aWords[1]} ideas of where it could be. Either next to the ${aWords[3]} ${nWords[2]} ` , 
-myStory4 = ` or by the ${nWords[3]} and ${nWords[4]}. `, 
-myStory5 = `They ${vWords[1]} for many ${aWords[2]} days. They could not find the long lost Treasure of Titan. Till one day, a message in a bottle, `,   
-myStory6 = `came to their aid. Their eyes ${vWords[2]} them forward when the message guided them to the long lost Treasure of Titan, which was with their ${nWords[1]}s named ${nWords[5]} and ${nWords[6]}, all along. ` ; 
+const myStory1 = `Once upon a time, there were four ${adjsWords[0]} ${nounWords[0]}s named ${chars[0]}andyn, ${chars[0]}yler, ${chars[0]}avin, and ${chars[0]}andie. `,
+myStory2 = `They were best friends for life and wanted a ${adjsWords[1]} adventure. They found a map for the` ,   
+myStory3 = ` lost Treasure of Titan. They searched for it by ${verbsWords[0]}ing for many ${adjsWords[2]} days. ` , 
+myStory4 = `They also looked by the ${nounWords[1]}, ${nounWords[2]}, and ${nounWords[3]}. `, 
+myStory5 = `They could not ${verbsWords[1]} to the long lost Treasure of Titan. Till one day, a message in a bottle, `,   
+myStory6 = `came to their aid. Their eyes ${verbsWords[2]}ed them forward when the message guided them to the long lost Treasure of Titan, which was with their ${nounWords[4]}s named ${nounWords[5]} and ${nounWords[6]}, all along. ` ; 
 
 
 
